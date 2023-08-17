998d6d15d0509628ef5ac75bf200434be65ae0c1
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-17 12:04:25 -0700

    Clear console on each run

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.clear()
 ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />,
 )
