b85870b783f051f8fd778bfad26cacb7b06c53f2
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-18 16:55:19 -0700

    Rework TextField

---
+++ src/components/TextField/MUI/TextField.tsx
@@ -0,0 +1,17 @@
+import { TextField as MuiTextField, TextFieldProps as MuiProps } from '@mui/material'
+type ReactProps = React.JSX.IntrinsicElements['input']
+type TextFieldProps = ReactProps | MuiProps
+
+export const TextField = (props: TextFieldProps) => {
+  const muiProps = props as MuiProps
+  return (
+    <MuiTextField
+      type="text"
+      id="unknown"
+      name="unknown"
+      size="medium"
+      variant="standard"
+      {...muiProps}
+    />
+  )
+}
---
+++ src/components/TextField/React/TextField.tsx
@@ -0,0 +1,15 @@
+import { TextFieldProps as MuiProps } from '@mui/material'
+type ReactProps = React.JSX.IntrinsicElements['input']
+type TextFieldProps = ReactProps | MuiProps
+
+export const TextField = (props: TextFieldProps) => {
+  const reactProps = props as ReactProps
+  const muiProps = props as MuiProps
+  const labelComponent = muiProps.label ? <label>{muiProps.label}</label> : null
+  return (
+    <div>
+      {labelComponent ?? null}
+      <input type="text" minLength={1} maxLength={80} id="unknown" name="unknown" {...reactProps} />
+    </div>
+  )
+}
