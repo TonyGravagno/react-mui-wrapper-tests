fc36f6a4ee857022b0ebe01179f54c3d10108d80
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-18 16:45:24 -0700

    Initial Vite for dynamic components (not working)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+type AppLibrary = 'MUI' | 'React' | undefined
+const appLibrary: AppLibrary = 'MUI'
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    {
+      name: 'library-resolver',
+      resolveId(source) {
+        return source.replace(/Library/, appLibrary)
+      },
+    },
+  ],
 })
