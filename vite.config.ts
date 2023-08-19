730c0735bc86e7fef5bb463bb19099b0955b6180
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-19 10:34:25 -0700

    Vite dynamic module selection - WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path';
 
 type AppLibrary = 'MUI' | 'React' | undefined
 const appLibrary: AppLibrary = 'MUI'
@@ -11,9 +12,25 @@ export default defineConfig({
     tsconfigPaths(),
     {
       name: 'library-resolver',
-      resolveId(source) {
-        return source.replace(/Library/, appLibrary)
+      resolveId(source, importer) {
+        const original = source;
+        const replaced = source.replace(/@DynLibName/, appLibrary);
+
+        if (replaced !== original) {
+          // If the importer is provided and the source is relative, resolve the full path
+          if (importer && source.startsWith('.')) {
+            const fullPath = path.resolve(path.dirname(importer), replaced) + '.tsx';
+//            this.warn(`From  : ${original}`);
+//            this.warn(`To    : ${fullPath}`);
+            return fullPath;
+          }
+
+//          this.warn(`From: ${original}`);
+//          this.warn(`To  : ${replaced}`);
+          return replaced;
+        }
+        return null;
       },
     },
   ],
-})
+});
