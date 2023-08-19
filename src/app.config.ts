8a5581647b68b7cc5c60a7c5c905672eaa3f7e7c
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-18 18:43:48 -0700

    Rework and improve config

---
+++ src/app.config.ts
@@ -0,0 +1,40 @@
+import { TextFieldProps, ButtonProps } from '@mui/material'
+type ReactProps = React.JSX.IntrinsicElements
+
+export type AppConfigJson = {
+  ui: {
+    toolkit: 'mui' | 'react'
+    button_size: ButtonProps['size']
+    button_variant: ButtonProps['variant']
+    text_variant: TextFieldProps['variant']
+    text_size: TextFieldProps['size']
+    textarea_maxRows: TextFieldProps['maxRows']
+    input_minLength: ReactProps['input']['minLength']
+    input_maxLength: ReactProps['input']['maxLength']
+    textarea_minLength: ReactProps['textarea']['minLength']
+    textarea_maxLength: ReactProps['textarea']['maxLength']
+    textarea_rows: ReactProps['textarea']['rows']
+    submit_text : string
+    id_name : string
+  }
+  status: string
+}
+
+export const config: AppConfigJson = {
+  ui: {
+    toolkit: 'react',
+    id_name: 'dunno',
+    button_variant: 'contained',
+    text_variant: 'standard',
+    button_size: 'medium',
+    text_size: 'medium',
+    textarea_maxRows: 5,
+    textarea_rows: 3,
+    textarea_minLength: 1,
+    textarea_maxLength: 20,
+    input_minLength: 0,
+    input_maxLength: 80,
+    submit_text: 'Click Me...'
+  },
+  status: 'Testing',
+}
