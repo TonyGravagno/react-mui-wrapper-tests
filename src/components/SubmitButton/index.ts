d6844ecc448f8c192fe2dcb147baaaace24f3c4e
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-23 09:28:08 -0700

    New way to dynamically choose components

--- src/components/SubmitButton/index.ts
+++ src/components/SubmitButton/index.ts
@@ -1,3 +1,17 @@
-// Required by code that uses @components/Name
-export * from './MUI/SubmitButton'
-//export * from '@types/commonTypes';
+import { config } from '@root/app.config'
+import { SubmitButton as C1 } from './MUI/SubmitButton'
+import { SubmitButton as C2 } from './React/SubmitButton'
+
+let C0
+switch (config.ui.toolkit) {
+  case 'mui':
+    C0 = C1
+    break
+  case 'react':
+    C0 = C2
+    break
+  default:
+    throw new Error(`Invalid toolkit ${config.ui.toolkit}`)
+    break
+}
+export const SubmitButton = C0
--- src/components/TextArea/index.ts
+++ src/components/TextArea/index.ts
@@ -1,3 +1,17 @@
-// Required by code that uses @components/Name
-export * from './MUI/TextArea'
-//export * from '@types/commonTypes';
+import { config } from '@root/app.config'
+import { TextArea as C1 } from './MUI/TextArea'
+import { TextArea as C2 } from './React/TextArea'
+
+let C0
+switch (config.ui.toolkit) {
+  case 'mui':
+    C0 = C1
+    break
+  case 'react':
+    C0 = C2
+    break
+  default:
+    throw new Error(`Invalid toolkit ${config.ui.toolkit}`)
+    break
+}
+export const TextArea = C0
