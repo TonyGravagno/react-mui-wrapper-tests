229d1d7e754943be27bf6fea7fa2a77e7e429dac
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-08-23 09:31:27 -0700

    Use new label determination function

--- src/components/TextArea/React/TextArea.tsx
+++ src/components/TextArea/React/TextArea.tsx
@@ -1,16 +1,22 @@
 import { TextFieldProps as MuiProps } from '@mui/material'
+import { getLabelForComponent } from '@components/Label'
 import { config } from '@root/app.config'
-//const config = AppConfig()
+import CSS from 'csstype'
+const spanStyle : CSS.Properties = {
+  marginRight: '23px',
+  verticalAlign: 'text-top',
+  textAlign: 'start'
+}
 type ReactProps = React.JSX.IntrinsicElements['textarea']
 type TextAreaProps = ReactProps & MuiProps
 
 export const TextArea = (props: TextAreaProps) => {
   const reactProps = props as ReactProps
   const muiProps = props as MuiProps
-  const labelComponent = muiProps.label ? <label>{muiProps.label}</label> : null
+  const labelComponent = getLabelForComponent(muiProps.label)
   return (
-    <div>
-      {labelComponent ?? null}
+    <div style={spanStyle}>
+      {labelComponent}
       <textarea
         id={config.ui.id_name}
         name={config.ui.id_name}
--- src/components/TextField/React/TextField.tsx
+++ src/components/TextField/React/TextField.tsx
@@ -1,4 +1,5 @@
 import { TextFieldProps as MuiProps } from '@mui/material'
+import { getLabelForComponent } from '@components/Label'
 import { config } from '@root/app.config'
 type ReactProps = React.JSX.IntrinsicElements['input']
 type TextFieldProps = ReactProps & MuiProps
@@ -6,10 +7,10 @@ type TextFieldProps = ReactProps & MuiProps
 export const TextField = (props: TextFieldProps) => {
   const reactProps = props as ReactProps
   const muiProps = props as MuiProps
-  const labelComponent = muiProps.label ? <label>{muiProps.label}</label> : null
+  const labelComponent = getLabelForComponent(muiProps.label)
   return (
     <div>
-      {labelComponent ?? null}
+      {labelComponent}
       <input
         type="text"
         minLength={config.ui.input_minLength}
