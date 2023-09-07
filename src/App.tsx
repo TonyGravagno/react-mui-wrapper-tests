cc5d25ff952a4038c12d0e1a8fd101b2911e70ad
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-09-07 13:28:33 -0700

    Switch App to use new PersonForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import {TestForm08 as TestForm} from '@forms/TestForm08'
+//import {TestForm08 as TestForm} from '@forms/TestForm08'
+import { entityMaintForm as PersonForm} from './wrappers/Person/PersonMaintForm'
 function App() {
   return (
     <>
-      <h2>T08</h2>
-      <TestForm />
+      <h2>entityMaintForm</h2>
+      <PersonForm />
     </>
   )
 }
