e3d078ef4cfd649ea3190e309ecdc8cd59450c69
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-09-08 16:52:33 -0700

    Use correct entity form name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 //import {TestForm08 as TestForm} from '@forms/TestForm08'
-import { entityMaintForm as PersonForm} from './wrappers/Person/PersonMaintForm'
+import { entityMaintForm as PersonMaintForm} from './entities/Person/PersonMaintForm'
 function App() {
   return (
     <>
       <h2>entityMaintForm</h2>
-      <PersonForm />
+      <PersonMaintForm />
     </>
   )
 }
