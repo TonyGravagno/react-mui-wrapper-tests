222326a278565b3b64e00ad9aaa1d44bbf73d855
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-09-13 14:19:22 -0700

    First implementation of Label

--- src/components/FieldLabel.tsx
+++ src/components/FieldLabel.tsx
@@ -1,107 +1,7 @@
 import { config } from '@root/app.config'
 import React from 'react'
-import { Controller, useFormContext } from 'react-hook-form'
-import { useSchema } from './SchemaContext'
-import { TextFieldProps as FieldTextProps } from '@apptypes/input'
-import { LabelProps, MuiProps } from '@apptypes/label'
-
-import {
-  FilledInput,
-  FilledInputProps,
-  Input,
-  InputLabel as MuiLabel,
-  InputProps,
-  OutlinedInput,
-  OutlinedInputProps,
-  FormControl,
-  FormLabel,
-  TextField as MuiTextField,
-  TextFieldProps as MuiTextFieldProps,
-} from '@mui/material'
-
-const controlType = 'text'
-
-export const FieldText: React.FC<FieldTextProps> = ({
-  name,
-  label,
-  variant = 'outlined',
-  ...otherProps
-}) => {
-  const {
-    control,
-    formState: { errors },
-  } = useFormContext()
-
-  const schema = useSchema()
-  const effectiveLabel = label || schema._def.description || ''
-  const effectiveId = otherProps.id || name
-  const onChangeEnabled = otherProps.onBlur !== undefined
-
-  const defaultProps = {
-    id: effectiveId,
-    name: name,
-    type: controlType,
-    size: config.ui.text_size,
-    style: config.styles.mui.formFieldInput,
-    label: effectiveLabel,
-  }
-
-  const renderInput = (field: any) => {
-    const commonProps = {
-      ...defaultProps,
-      ...field,
-      error: !!errors[name],
-      helperText: errors[name] ? <span>{String(errors[name]?.message)}</span> : null,
-    }
-
-    switch (variant) {
-      case 'filled':
-        return <FilledInput {...commonProps} {...(otherProps as FilledInputProps)} />
-      case 'outlined':
-        return <OutlinedInput {...commonProps} {...(otherProps as OutlinedInputProps)} />
-      case 'standard':
-      default:
-        return <Input {...commonProps} {...(otherProps as InputProps)} />
-    }
-  }
-
-  return (
-    <Controller
-      name={name}
-      control={control}
-      shouldUnregister={onChangeEnabled}
-      rules={{
-        validate: value => {
-          const result = schema.safeParse(value)
-          if (result?.success) {
-            return true
-          } else {
-            return result?.error?.errors[0]?.message || 'Invalid value'
-          }
-        },
-      }}
-      render={({ field }) =>
-        typeof effectiveLabel === 'string' ? (
-          <MuiTextField
-            {...defaultProps}
-            variant={variant}
-            label={effectiveLabel}
-            {...field}
-            {...(otherProps as MuiTextFieldProps)}
-            error={!!errors[name]}
-            helperText={errors[name] ? <span>{String(errors[name]?.message)}</span> : null}
-          />
-        ) : (
-          <FormControl variant={variant}>
-            <FormLabel>{effectiveLabel}</FormLabel>
-            {renderInput(field)}
-          </FormControl>
-        )
-      }
-    />
-  )
-}
-
+import { LabelProps } from '@apptypes/label'
+import { InputLabel as MuiLabel } from '@mui/material'
 
 export type LabelForComponentOptions = {
   props?: LabelProps
@@ -116,7 +16,7 @@ export const getLabelForComponent = (
     case 'number':
     case 'bigint':
     case 'boolean':
-      return <Label {...(options?.props ?? {})}>{field}</Label>
+      return <FieldLabel {...options?.props}>{field}</FieldLabel>
     case 'function':
     case 'object':
     case 'symbol':
@@ -128,8 +28,12 @@ export const getLabelForComponent = (
   }
 }
 
-export const Label = (props: LabelProps) => {
-  const muiProps = props as MuiProps
-  const style = muiProps.style ?? config.styles.mui.formFieldLabel
-  return <MuiLabel {...muiProps} style={style} />
+export const FieldLabel: React.FC<LabelProps> = (props: LabelProps) => {
+  return (
+    <MuiLabel
+      // provide defaults and let props override
+      style={config.styles.mui.formFieldLabel}
+      {...props}
+    />
+  )
 }
